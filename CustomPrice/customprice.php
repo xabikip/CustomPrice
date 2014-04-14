acb4d7d09c9b38611e331fa9d7e8d76f78e5401e
Xabi Pico <xabikip@gmail.com>
2014-04-14 11:48:42 +0200

    pequeño fallo

--- CustomPrice/customprice.php
+++ CustomPrice/customprice.php
@@ -1,11 +1,11 @@
 <?php
 /*
-*  2014 
+*  2014
 *
 *  @author   Xabi Pico <xabikip@gmail.com>
 *  @version  Release: 0.1
-*  @license  http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)    
-*  
+*  @license  http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*
 */
 
 if (!defined('_PS_VERSION_'))
@@ -22,15 +22,15 @@ class Customprice extends Module
 	    $this->author = 'Xabi Pico';
 	    $this->need_instance = 0;
 	    $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.5.6.2');
-	 
+
 	    parent::__construct();
-	 
+
 	    $this->displayName = $this->l('CustomPrice');
 	    $this->description = $this->l('Calcula el precio en base a parametros custom');
-	 
+
 	    $this->confirmUninstall = $this->l('Esta seguro que desea desinstalar?');
-	 
-	    if (!Configuration::get('CUSTOM_PRICE'))      
+
+	    if (!Configuration::get('CUSTOM_PRICE'))
 	      $this->warning = $this->l('No name provided');
     }
 
@@ -45,22 +45,22 @@ class Customprice extends Module
 	public function getContent()
 	{
 	    $output = null;
-	 
+
 	    if (Tools::isSubmit('submit'.$this->name))
 	    {
 	        $precio_carac_nombre = strval(Tools::getValue('precio_carac_nombre'));
-	        $precio_carac_num_ad = strval(Tools::getValue('precio_carac_num'));
+	        $precio_carac_num = strval(Tools::getValue('precio_carac_num'));
 	        $carac_max = strval(Tools::getValue('carac_max'));
 	        $num_max = strval(Tools::getValue('num_max'));
 	        if (!$precio_carac_nombre  || empty($precio_carac_nombre) || !Validate::isGenericName($precio_carac_nombre) and
-	        	!$precio_carac_num_ad  || empty($precio_carac_num) || !Validate::isGenericName($precio_carac_num_ad) and
+	        	!$precio_carac_num  || empty($precio_carac_num) || !Validate::isGenericName($precio_carac_num) and
 	        	!$carac_max  || empty($carac_max) || !Validate::isGenericName($carac_max) and
 	        	!$num_max  || empty($num_max) || !Validate::isGenericName($num_max))
 	            $output .= $this->displayError( $this->l('Invalid Configuration value') );
 	        else
 	        {
 	            Configuration::updateValue('precio_carac_nombre', $precio_carac_nombre);
-	            Configuration::updateValue('precio_carac_num_ad', $precio_carac_num);
+	            Configuration::updateValue('precio_carac_num', $precio_carac_num);
 	            Configuration::updateValue('carac_max', $carac_max);
 	            Configuration::updateValue('num_max', $num_max);
 	            $output .= $this->displayConfirmation($this->l('Settings updated'));
@@ -68,12 +68,12 @@ class Customprice extends Module
 	    }
 	    return $output.$this->displayForm();
 	}
-	
+
 	public function displayForm()
 	{
 	    // Get default Language
 	    $default_lang = (int)Configuration::get('PS_LANG_DEFAULT');
-	     
+
 	    // Init Fields form array
 	    $fields_form[0]['form'] = array(
 	        'legend' => array(
@@ -116,19 +116,19 @@ class Customprice extends Module
 	            'class' => 'button'
 	        )
 	    );
-	     
+
 	    $helper = new HelperForm();
-	     
+
 	    // Module, token and currentIndex
 	    $helper->module = $this;
 	    $helper->name_controller = $this->name;
 	    $helper->token = Tools::getAdminTokenLite('AdminModules');
 	    $helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
-	     
+
 	    // Language
 	    $helper->default_form_language = $default_lang;
 	    $helper->allow_employee_form_lang = $default_lang;
-	     
+
 	    // Title and toolbar
 	    $helper->title = $this->displayName;
 	    $helper->show_toolbar = true;        // false -> remove toolbar
@@ -146,30 +146,30 @@ class Customprice extends Module
 	            'desc' => $this->l('Back to list')
 	        )
 	    );
-	     
+
 	    // Load current value
 	    $helper->fields_value['precio_carac_nombre'] = Configuration::get('precio_carac_nombre');
-	    $helper->fields_value['precio_carac_num_ad'] = Configuration::get('precio_carac_num');
+	    $helper->fields_value['precio_carac_num'] = Configuration::get('precio_carac_num');
 	    $helper->fields_value['carac_max'] = Configuration::get('carac_max');
 	    $helper->fields_value['num_max'] = Configuration::get('num_max');
-	     
+
 	    return $helper->generateForm($fields_form);
 	}
-    
+
 
 	public function hookdisplayRightColumnProduct($params){
 
         $this->context->smarty->assign(
 	        array(
 	            'precio_carac_nombre' => Configuration::get('precio_carac_nombre'),
-	            'precio_carac_num_ad' => Configuration::get('precio_carac_num'),
+	            'precio_carac_num' => Configuration::get('precio_carac_num'),
 	            'carac_max' => Configuration::get('carac_max'),
 	            'num_max' => Configuration::get('num_max')
 	        )
         );
         return $this->display(__FILE__, 'customprice.tpl');
-		
+
 	}
 
-	
+
 }
--- CustomPrice/views/templates/hook/customprice.tpl
+++ CustomPrice/views/templates/hook/customprice.tpl
@@ -2,34 +2,34 @@
 <div id="custom-price">
   <h4>Precios por caracter:</h4>
   <div class="block_content">
-    Cada caracter del texto: 
+    Cada caracter del texto:
        {if isset($precio_carac_nombre) && $precio_carac_nombre}
            {$precio_carac_nombre}€
        {else}
            Ez dago!
-       {/if}       
+       {/if}
     <br>
-    Cada caracter del numero: 
-       {if isset($precio_carac_num_ad) && $precio_carac_num_ad}
-           {$precio_carac_num_ad}€
+    Cada caracter del numero:
+       {if isset($precio_carac_num) && $precio_carac_num}
+           {$precio_carac_num}€
        {else}
            Ez dago!
-       {/if} 
+       {/if}
     <br>
     Maximo de caracteres para texto:
        {if isset($carac_max) && $carac_max}
            <span class="textMax">{$carac_max} </span> Caracteres
        {else}
            Ez dago!
-       {/if} 
+       {/if}
     <br>
     Maximo de numeros:
        {if isset($num_max) && $num_max}
            <span class="numMax">{$num_max} </span> Numeros
        {else}
            Ez dago!
-       {/if}       
-         
+       {/if}
+
   </div>
 </div>
 <!-- /Block mymodule -->
